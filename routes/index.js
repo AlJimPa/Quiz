e54763164c96426cfc75a472fa8bfc78958691ad
AlJimPa <igorman84@gmail.com>
2015-06-12 17:48:40 +0200

    Paso 11: Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,9 @@ router.param('quizId', quizController.load); //autoload :quizId
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+//páginas de edición de preguntas
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 
 //página de créditos
 router.get('/author', function(req, res) {
