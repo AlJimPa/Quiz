b1843ad50e305445c72747f5813abc4e3e2f0010
AlJimPa <igorman84@gmail.com>
2015-06-19 16:39:53 +0200

    7b2dbe9

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -3,7 +3,7 @@ var models = require('../models/models.js');
 //autoload
 exports.load = function(req, res, next, commentId) {
 	models.Comment.find({
-		where : {Id : Number(commentId)}
+		where : {id : Number(commentId)}
 	}).then(
 		function(comment) {
 			if (comment){
